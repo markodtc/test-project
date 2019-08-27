50cbbe48c62ead710707a556a093c390afc608d5
Marko Markovic <marko.markovic@dtc.rs>
2019-08-27 09:44:38 +0200

    nova test faza

---
+++ build.bat
@@ -0,0 +1,19 @@
+set apiDir=C:\Users\marko\Work\Git\WSO2\PSD2_sandbox\API\
+set carDir=C:\wso2ei-6.2.0\wso2ei-6.2.0\repository\deployment\server\carbonapps
+
+set apiNames=AccountInformationV1 CardAccountInformationV1 FundsConfirmationV1 PaymentInitiationV1
+
+for %%a in (%apiNames%) do (
+	
+	cd /d %apiDir%%%a
+
+	call mvn clean install
+
+	cd /d %apiDir%%%a_Registry
+
+	call mvn clean install
+
+	cd /d %apiDir%%%a_CompositeApplication
+
+	call mvn clean install
+)
